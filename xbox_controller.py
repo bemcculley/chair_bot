ee213377e79e58d49be18e0b8723c3162e9e9d32
Brian McCulley <brian.mcculley@gmail.com>
2013-10-16 23:53:36 -0600

    adding led dimming

--- xbox_controller.py
+++ xbox_controller.py
@@ -16,6 +16,12 @@ class XboxHandler(object):
         self.j.init()
         print 'Initialized Joystick : %s' % self.j.get_name()
 
+        self.green = 255
+        self.red = 255
+        self.blue = 255
+
+
+
 
     def joystickHandler(self):
         try:
@@ -23,6 +29,8 @@ class XboxHandler(object):
                 pygame.event.pump()
                 event = pygame.event
                 for thing in event.get():
+                    print thing.type
+                    print thing
                     if thing.type == 7:
                         try:
                             #Joystick 1 (left)
@@ -34,29 +42,65 @@ class XboxHandler(object):
                                 #X Axis?
                                 
                                 self.control.drive(1, thing.dict['value'])
+
+
                         except Exception, e:
                             print e
                         print
-                    if thing.type == 10:
+                    if thing.type == 9 or thing.type == 10:
                         self.buttonHandler(thing)
 
         except KeyboardInterrupt:
             self.j.quit()
 
     def buttonHandler(self,thing):
+        print self.green
         #Mappings for xbox buttons, trigger on depress
         # 0 = A, 1 = B, 2 = Y, 3 = X
-        if thing.dict['button'] == 0:
-            self.control.drive(0x05, 255)
-        if thing.dict['button'] == 1:
-            self.control.drive(0x03, 255)
-        if thing.dict['button'] == 3:
-            self.control.drive(0x06, 000)
-        if thing.dict['button'] == 2:
-            self.control.drive(0x04, 255)
 
-        print 'Button: %s pressed' % thing.dict['button']
-        print
+        if thing.type == 10:
+            if thing.dict['button'] == 0:
+                self.control.drive(0x05, self.green)
+
+            if thing.dict['button'] == 1:
+                self.control.drive(0x03, self.red)
+
+            if thing.dict['button'] == 3:
+                self.control.drive(0x06, 000)
+
+            if thing.dict['button'] == 2:
+                self.control.drive(0x04, self.blue)
+
+            print 'Button: %s pressed' % thing.dict['button']
+            print
+
+        if thing.type == 9:
+            if self.j.get_button(0) == 1:
+                #green
+                self.green = self.green + (thing.dict['value'][1] * 25)
+                if self.green > 255:
+                    self.green = 255
+                if self.green < 0:
+                    self.green = 0
+                self.control.drive(0x05, self.green)
+            if self.j.get_button(1) == 1:
+                #red
+                self.red = self.red + (thing.dict['value'][1] * 25)
+                if self.red > 255:
+                    self.red = 255
+                if self.red < 0:
+                    self.red = 0
+                self.control.drive(0x03, self.red)
+            if self.j.get_button(2) == 1:
+                #blue
+                self.blue = self.blue + (thing.dict['value'][1] * 25)
+                if self.blue > 255:
+                    self.blue = 255
+                if self.blue < 0:
+                    self.blue = 0
+                self.control.drive(0x04, self.blue)
+
+
         if thing.type == 11:
             print 'Button: %s released' % thing.dict['button']
         print
